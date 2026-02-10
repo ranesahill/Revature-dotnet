4f3c9603ad427abf4cc6ad7d54d2be91b541bdda
ranesahill <sahilrane.skn.it@gmail.com>
2026-02-10 08:50:46 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,24 +1,56 @@
-namespace Day2Assignments
-{
-    public class Program
-    {
-        public static void Main()
-        {
-            Section01_Intro.Run();
-            Section02_EntryPoints.Run();
-            Section03_Primitives.Run();
-            Section04_Strings.Run();
-            Section05_Var.Run();
-            Section06_Nullables.Run();
-            Section07_Conditionals.Run();
-            Section08_Patterns.Run();
-            Section09_LogicalOps.Run();
-            Section10_Math.Run();
-            Section11_Collections.Run();
-            Section12_Loops.Run();
-            Section13_Comments.Run();
-            Section14_Namespaces.Run();
-            Section15_Debugging.Run();
-        }
-    }
-}
+﻿using System;
+// System.Console.WriteLine(StaticClassDemo.StaticClassConstant);
+
+
+// var x = new ClassVsObject();
+// System.Console.WriteLine(x.MyProperty);
+
+
+// PropertiesDemo demo = new PropertiesDemo();
+// Console.WriteLine(demo.MyProperty);
+// // demo.MyProperty = 50; // This will cause a compile-time error
+
+// Console.WriteLine($"Age before modification: {demo.MyAge}");
+// // demo.MyAge = 25; // This will cause a compile-time error
+// demo.ModifyName();
+// Console.WriteLine($"Age before modification: {demo.MyAge}");
+
+// ElectricityOnlySupplier supplier = new ElectricityOnlySupplier();
+
+// GenerateBill(supplier);
+
+// void GenerateBill(IElectricityProvider electricityProvider)
+// {
+//     electricityProvider.SupplyService();
+//     internetProvider.Supply();
+// }
+
+
+Fruit discount = new Apple();
+
+discount.ApplyDiscount();
+
+
+//Fruit d = new Fruit();
+
+//IElectricityProvider x = new IElectricityProvider();
+
+
+//abstract class Car
+//{ 
+//}
+
+//abstract class IMusicPlayer
+//{
+//}
+
+//class Swift : Car, IHaveWings, IMUsicPlayer
+//{
+//    Body body;
+//    IEnumerable<Tire> Tiers;
+//    MusicPlayer musicPlayer;
+//}
+
+//// is-a
+//// is part of
+//// has-a
